abf51284626cb42ca39294d3daa2cc69fd3cdb03
IEUser <IEUser@192.168.190.139>
2018-04-24 01:51:09 -0700

    "El verdadero KODIGO"

--- Hola Git/src/HolaGit.java
+++ Hola Git/src/HolaGit.java
@@ -4,6 +4,10 @@ public class HolaGit {
 	public static void main(String[] args) {
 		System.out.println("Hola Git te queremo mucho");
 		System.out.println("Pues eso hola de novo");
+		System.out.println("Santi carawea se pilla skin de wukong porque juega con un simio :v");
+		System.out.println("Mesa esta usted hecho de roble como disen las gaviotas?");
+
+
 	}
 
 }
